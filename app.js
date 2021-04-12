7a74458ff44da15c4b47b8945961c25c925e65e2
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-12 11:03:49 +0530

    Socket Connection

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const path = require('path')
-const http = require('http')
 const express = require('express')
 
 const app = express()
-// const server = http.createServer(app)
+
 const server = app.listen(3000, () => {
   console.log('Server is up and running`')
 })
