da18b8dd533b6e8f46ef0ca94203c9cd3dbcfdd6
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-12 09:43:47 +0530

    Socket Connection

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 
 const app = express()
+const server = http.createServer(app)
+
+const io = require('./socket').init(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Run when the client connects
+io.on('connection', socket => {
+  console.log('New web socket connection')
+})
 
 app.listen(3000, () => {
   console.log('Server is up and running`')
